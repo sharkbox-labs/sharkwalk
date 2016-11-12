f0ad60d0374015e9e93ee1c4875b88471291379e
Ker Moua <kermoua92@gmail.com>
2016-11-11 19:55:28 -0800

    Add 2 tests for / and /api endpoints

--- integrationServer/spec/serverSpec.js
+++ integrationServer/spec/serverSpec.js
@@ -3,7 +3,15 @@ const expect = require('chai').expect;
 const app = require('../server');
 
 describe('Integration Server', () => {
-  it('', () => {
-    
+  it('should respond with a 200 status code on / GET', (done) => {
+    request(app)
+      .get('/')
+      .expect(200, done);
+  });
+
+  it('should respond with a 200 status code on /api GET', (done) => {
+    request(app)
+      .get('/api')
+      .expect(200, done);
   });
 });
