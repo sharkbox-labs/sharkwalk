82db26c1774617d77672fdf9ddfc281e8b707340
Ker Moua <kermoua92@gmail.com>
2016-11-14 11:41:05 -0800

    (feature) Change file path for static files

--- integrationServer/server.js
+++ integrationServer/server.js
@@ -11,7 +11,7 @@ app.use(morgan('dev'));
 // To handle CORS
 app.use(cors());
 
-app.use('/', express.static(path.join(__dirname, '../client')));
+app.use('/', express.static(path.join(__dirname, '../client/build')));
 
 app.use('/api', router);
 
