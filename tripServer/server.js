4242ab6742d1e469f1ace945bff95a98e97b33c7
Analisa Heilmann <analisa.heilmann@gmail.com>
2016-11-15 11:48:34 -0800

    Add port.

--- tripServer/server.js
+++ tripServer/server.js
@@ -10,7 +10,7 @@ app.get('/api/trip', requestHandler);
 const port = 3001;
 
 app.listen(port, () => {
-  console.log('listening...');
+  console.log('listening on port 3001...');
 });
 
 module.exports = app;
