65ea089343c64a6fb8b43b429664202b80243693
David Walsh <dawalsh@gmail.com>
2016-11-15 14:39:45 -0800

    Refine spec to match new fetch

--- riskService/spec/dataConnectorSpec.js
+++ riskService/spec/dataConnectorSpec.js
@@ -1,9 +1,9 @@
 const expect = require('chai').expect;
 const sfData = require('../assessmentWorker/sanFranciscoDataConnector');
 
-/* eslint-disable no-unused-expressions */
+/* eslint-disable no-unused-expressions, arrow-body-style */
 
-xdescribe('San Francisco crime reports', () => {
+describe('San Francisco crime reports', () => {
   describe('Fetching crime reports', () => {
     const bounds = {
       type: 'FeatureCollection',
@@ -46,7 +46,7 @@ xdescribe('San Francisco crime reports', () => {
       return sfData.fetchCrimeReports(bounds, '1 month ago', 2)
         .then((reports) => {
           expect(reports).to.be.an('Array');
-          expect(reports[0]).to.include.keys(['pddistrict', 'x', 'y', 'resolution']);
+          expect(reports[0]).to.include.keys(['category', 'date', 'descript', 'location']);
         });
     });
 
@@ -54,7 +54,7 @@ xdescribe('San Francisco crime reports', () => {
       return sfData.fetchCrimeReports(bounds, '1 month ago')
         .then((reports) => {
           expect(reports).to.be.an('Array');
-          expect(reports[0]).to.include.keys(['pddistrict', 'x', 'y', 'resolution']);
+          expect(reports[0]).to.include.keys(['category', 'date', 'descript', 'location']);
         });
     });
   });
