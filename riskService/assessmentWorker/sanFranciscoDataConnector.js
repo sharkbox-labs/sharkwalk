880a10fb6bfae6d594b048e5866a175beddd606d
David Walsh <dawalsh@gmail.com>
2016-11-15 14:39:16 -0800

    Handle results beyond the API max

--- riskService/assessmentWorker/sanFranciscoDataConnector.js
+++ riskService/assessmentWorker/sanFranciscoDataConnector.js
@@ -52,9 +52,47 @@ const sfCrimeURL = 'https://data.sfgov.org/resource/cuks-n6tp.json';
  *    ],
  *  };
  * fetchCrimeReports(bounds, '2 weeks ago', 50)
- *   .then((reports) => { // do something with reports // });
+ *   .then((reports) => { console.log(reports) });
+ * // outputs:
+ * [
+ *   {
+ *     address: 'SACRAMENTO ST / JOICE ST',
+ *     category: 'LARCENY/THEFT',
+ *     date: '2016-10-15T00:00:00.000',
+ *     dayofweek: 'Saturday',
+ *     descript: 'GRAND THEFT FROM LOCKED AUTO',
+ *     incidntnum: '166234434',
+ *     location: { type: 'Point', coordinates: [ -122.396382, 37.781749 ] },
+ *     pddistrict: 'CENTRAL',
+ *     pdid: '16623443406244',
+ *     resolution: 'NONE',
+ *     time: '14:00',
+ *     x: '-122.408547479638',
+ *     y: '37.7930734224606'
+ *   },
+ *   {
+ *     address: 'MISSION ST / 4TH ST',
+ *     category: 'LARCENY/THEFT',
+ *     date: '2016-10-15T00:00:00.000',
+ *     dayofweek: 'Saturday',
+ *     descript: 'PETTY THEFT OF PROPERTY',
+ *     incidntnum: '166233925',
+ *     location: { type: 'Point', coordinates: [ -122.40427, 37.784479 ] },
+ *     pddistrict: 'SOUTHERN',
+ *     pdid: '16623392506372',
+ *     resolution: 'NONE',
+ *     time: '15:30',
+ *     x: '-122.404270179486',
+ *     y: '37.7844788538745'
+ *   }
+ * ]
  */
-const fetchCrimeReports = function fetchCrimeReports(bounds, lookback, limit = null) {
+const fetchCrimeReports = function fetchCrimeReports(
+  bounds,
+  lookback,
+  limit = 50000, // 50000 is the max the API will return
+  results = [],
+  offset = 0) {
   const sinceDate = (lookback ? sugar.Date(lookback) : sugar.Date('6 months ago'))
     .format('{yyyy}-{MM}-{dd}T{HH}:{mm}:{ss}.{ms}');
 
@@ -66,13 +104,25 @@ const fetchCrimeReports = function fetchCrimeReports(bounds, lookback, limit = n
   [minX, minY, maxX, maxY] = turf.bbox(envelope); // eslint-disable-line prefer-const
   return request(sfCrimeURL, {
     params: {
-      $where: `x >= ${minX} and x <= ${maxX} and y >= ${minY} and y <= ${maxY} and date > '${sinceDate}'`,
+      $where: `x >= ${minX} and x <= ${maxX} and y >= ${minY} and y <= ${maxY} and date > '${sinceDate}' and category != 'NON-CRIMINAL'`,
       $limit: limit,
+      $select: 'category, time, date, descript, location',
+      $offset: offset,
     },
   })
-  .then(response => response.data);
+  .then((response) => {
+    const newResults = [...results, ...response.data];
+    if (response.data.length === limit && limit === 50000) {
+      // We are at our max number of records and need to paginate
+      console.log(`Retrieved the limit of ${limit} records. Paginating.`);
+      return fetchCrimeReports(bounds, lookback, limit, newResults, offset + limit);
+    }
+    console.log(`Retrieved ${newResults.length} records.`);
+    return newResults;
+  });
 };
 
 module.exports = {
   fetchCrimeReports,
+  request,
 };
