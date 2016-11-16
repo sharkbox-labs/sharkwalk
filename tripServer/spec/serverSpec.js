6d6f27a2090e3b50d2904a2b6fe29289d405dbbe
Analisa Heilmann <analisa.heilmann@gmail.com>
2016-11-16 10:49:46 -0800

    Begin server side tests.

---
+++ tripServer/spec/serverSpec.js
@@ -0,0 +1,48 @@
+const request = require('supertest');
+const expect = require('chai').expect;
+const app = require('../server');
+
+// temporary empty query
+const testQuery = {};
+
+describe('Trip Server:', () => {
+  it('should respond with a 200 status code', (done) => {
+    request(app)
+      .get('/trip')
+      .expect(200, done);
+  });
+  it('should have property "path" in the response body', (done) => {
+    request(app)
+      .get('/trip')
+      .query(testQuery)
+      .end((error, response) => {
+        expect(error).to.not.exist;
+        expect(response.status).to.equal(200);
+        expect(response.body).to.have.property('path');
+        done();
+      });
+  });
+  it('value of path should be an array of coordinates', (done) => {
+    request(app)
+      .get('/trip')
+      .query(testQuery)
+      .end((error, response) => {
+        expect(error).to.not.exist;
+        expect(response.status).to.equal(200);
+        expect(response.body.path[0].length).to.equal(2);
+        expect(response.body.path[0][0]).to.be.a('number');
+        done();
+      });
+  });
+  it('should have property "route" in the response body', (done) => {
+    request(app)
+      .get('/trip')
+      .query(testQuery)
+      .end((error, response) => {
+        expect(error).to.not.exist;
+        expect(response.status).to.equal(200);
+        expect(response.body).to.have.property('route');
+        done();
+      });
+  });
+});
