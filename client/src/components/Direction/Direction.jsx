094165c33bda0222a63f0dfa4904672b187c1d38
mjstromberg <michaeljstromberg@gmail.com>
2016-11-15 18:00:43 -0800

    (feature) Add instance of google.maps.DirectionsService

--- client/src/components/Direction/Direction.jsx
+++ client/src/components/Direction/Direction.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 class Direction extends Component {
+  componentDidUpdate(prevProps) {
+    if (this.props.directionsResponse !== prevProps.directionsResponse) {
+      this.renderDirection();
+    }
+  }
+
+  renderDirection() {
+    const directionService = new google.maps.DirectionService();
+  }
+
   render() {
     return null;
   }
