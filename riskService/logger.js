b0a7254a64d142707c5a86d498707dff5868d402
David Walsh <dawalsh@gmail.com>
2016-11-15 21:50:05 -0800

    Correct path for log file

--- riskService/logger.js
+++ riskService/logger.js
@@ -3,6 +3,6 @@ const winston = require('winston');
 module.exports = new (winston.Logger)({
   transports: [
     new (winston.transports.Console)(),
-    new (winston.transports.File)({ filename: 'jellywave.log' }),
+    new (winston.transports.File)({ filename: './jellywave.log' }),
   ],
 });
