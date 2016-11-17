22d8d215591a37236973631fb6e484fea47f1fe4
David Walsh <dawalsh@gmail.com>
2016-11-16 22:09:05 -0800

    Check for throttling and log statuses with logger

---
+++ riskService/assessmentWorker/worker.js
@@ -0,0 +1,39 @@
+const shortid = require('shortid');
+const logger = require('../logger');
+const riskGenerator = require('./riskGenerator');
+const riskPointController = require('../db/riskPointController');
+const db = require('../db/connection');
+
+require('dotenv').config({ silent: true });
+
+/**
+ * Takes an area and generates the risk points within that area, saving the
+ * result to the database.
+ * @module worker
+ * @param  {GeoJSON Feature Collection} area - A GeoJSON feature collection
+ * representing the area bounds. Visit [geojson.io](http://geojson.io/) for
+ * an easy GUI to create the object.
+ * @return {Promise} Resolves to an array of the created records.
+ */
+module.exports = function worker(area) {
+  const batchId = shortid.generate();
+  return riskGenerator.generateRiskForArea(area)
+  .then((riskPoints) => {
+    logger.info(`Generated ${riskPoints.features.length} risk points`);
+    return riskPointController
+      .createRiskPoints(riskPoints.features.map(feature => ({
+        location: feature.geometry,
+        risk: feature.properties.risk,
+      })), batchId);
+  })
+  .then((records) => {
+    logger.info(`Saved ${records.length} risk points in batch ${batchId}`);
+    db.close();
+    return records;
+  })
+  .catch((error) => {
+    logger.error(`Error '${error.message}' with risk points batch ${batchId}`);
+    db.close();
+    throw error;
+  });
+};
