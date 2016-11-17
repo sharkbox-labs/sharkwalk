dc018ebd1f46f140488efa101ba05f48e5bf4595
Ker Moua <kermoua92@gmail.com>
2016-11-16 18:49:48 -0800

    (feature) Pass response from tripServer back to client
    Commented out code that would pass the response to the riskServer.
    Once riskServer is complete, should be good to go.

--- integrationServer/requestHandlers.js
+++ integrationServer/requestHandlers.js
@@ -2,25 +2,33 @@ const axios = require('axios');
 const qs = require('qs');
 
 const requestHandler = (request, response) => {
+  const tripServerUrl = 'http://localhost:3001';
+
+  const riskServerUrl = 'http://localhost:3002';
+
   const queryObj = qs.stringify(request.query);
 
   // Send query to trip server
-  axios.get(`/trip?${queryObj}`)
+  axios.get(`${tripServerUrl}/trip?${queryObj}`)
     .then((tripResponse) => {
-      const routeObj = qs.stringify(tripResponse.data);
-      // Pass response from trip server to risk server
-      axios.get(`/risk?${routeObj}`)
-        .then((riskResponse) => {
-          // Send fully decorated object back to client
-          response.status(200).json(riskResponse.data);
-        })
-        .catch((error) => {
-          response.status(502).json({
-            error: {
-              message: 'Failed to retrieve risk rating from risk server',
-            },
-          });
-        });
+      response.status(200).json(tripResponse.data);
+
+      // ====UNCOMMENT ONCE RISK SERVER IS COMPLETE=====
+
+      // const routeObj = qs.stringify(tripResponse.data);
+      // // Pass response from trip server to risk server
+      // axios.get(`${riskServerUrl}/risk?${routeObj}`)
+      //   .then((riskResponse) => {
+      //     // Send fully decorated object back to client
+      //     response.status(200).json(riskResponse.data);
+      //   })
+      //   .catch((error) => {
+      //     response.status(502).json({
+      //       error: {
+      //         message: 'Failed to retrieve risk rating from risk server',
+      //       },
+      //     });
+      //   });
     })
     .catch((error) => {
       response.status(502).json({
