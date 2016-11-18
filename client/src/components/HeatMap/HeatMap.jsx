9f2a51f774c9d1799743fd953db1083d1d9d0519
mjstromberg <michaeljstromberg@gmail.com>
2016-11-18 11:44:31 -0800

    (feature) Update HeatMap component to render a heat map on props update

--- client/src/components/HeatMap/HeatMap.jsx
+++ client/src/components/HeatMap/HeatMap.jsx
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
 import { GoogleApiWrapper } from 'google-maps-react';
 import googleApiKey from '../../apiKeys/googleApiKey';
-import heatmapData from './heatmapsampledata';
+// import heatmapData from './heatmapsampledata';
 
 class HeatMap extends Component {
-  // This function is only for displaying a direction during development
-  // Remove before production
-  componentWillMount() {
-    const points = heatmapData.map((point) => {
-      return { location: new google.maps.LatLng(point.location[0], point.location[1]), weight: point.weight }; // eslint-disable-line
+  // // This function is only for displaying a direction during development
+  // // Remove before production
+  // componentWillMount() {
+  //   const points = heatmapData.map(point => (
+  //     {
+  //       location: new google.maps.LatLng(point.location[0], point.location[1]), // eslint-disable-line
+  //       weight: point.weight,
+  //     }
+  //   ));
+
+  //   const heatmap = new google.maps.visualization.HeatmapLayer({ data: points, radius: 25, }); // eslint-disable-line
+
+  //   heatmap.setMap(this.props.map);
+  // }
+
+  // When the componenet updates, if the props have changed call the renderHeatMap method
+  componentDidUpdate(prevProps) {
+    if (this.props.heatMapResponse !== prevProps.heatMapResponse) {
+      this.renderHeatMap();
+    }
+  }
+
+  // Updates the Google Map with a new Heat Map
+  renderHeatMap() {
+    // Reformat heatMapResponse to the format required by Google Maps
+    const points = this.props.heatMapResponse.map(point => ({
+      location: new google.maps.LatLng(point.location[0], point.location[1]), // eslint-disable-line
+      weight: point.weight,
+    }));
+
+    // Instantiate the Google Maps Visualization Heat Map Layer with the data and radius
+    const heatmap = new google.maps.visualization.HeatmapLayer({ // eslint-disable-line
+      data: points,
+      radius: 25,
     });
 
-    const heatmap = new google.maps.visualization.HeatmapLayer({ data: points, radius: 25, }); // eslint-disable-line
+    // Associate the Heat Map with the current map
     heatmap.setMap(this.props.map);
   }
 
@@ -22,6 +51,7 @@ class HeatMap extends Component {
 }
 
 HeatMap.propTypes = {
+  heatMapResponse: React.PropTypes.array, // eslint-disable-line
   map: React.PropTypes.object, // eslint-disable-line
 };
 
