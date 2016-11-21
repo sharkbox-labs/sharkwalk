25f337a29e5aa549b3440f824e64300e7be137ca
mjstromberg <michaeljstromberg@gmail.com>
2016-11-20 16:57:23 -0800

    (refactor) Enlarge heat map point radius

--- client/src/components/utils/heatMapRadiusHelper.js
+++ client/src/components/utils/heatMapRadiusHelper.js
@@ -37,7 +37,7 @@ const getNewRadius = (map, google, TILE_SIZE) => {
     };
   }
 
-  const desiredRadiusPerPointInMeters = 20;
+  const desiredRadiusPerPointInMeters = 30;
   const numTiles = 1 << map.getZoom(); // eslint-disable-line
   const center = map.getCenter();
   // 1000 meters to the right
