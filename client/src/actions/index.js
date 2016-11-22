1353a944ae90a83d6112efa9a3e634277d9d2116
mjstromberg <michaeljstromberg@gmail.com>
2016-11-22 14:00:47 -0800

    (refactor) Add initial action creators
    Create directory for actions and build out initial action creators for Search and Map components

---
+++ client/src/actions/index.js
@@ -0,0 +1,27 @@
+export const setDestination = (destination) => {
+  return {
+    type: 'SET_DESTINATION',
+    destination,
+  };
+};
+
+export const setOrigin = (origin) => {
+  return {
+    type: 'SET_ORIGIN',
+    origin,
+  };
+};
+
+export const setInteractionType = (interactionType) => {
+  return {
+    type: 'SET_INTERACTION_TYPE',
+    interactionType,
+  };
+};
+
+export const setCurrentRoute = (routeIndex) => {
+  return {
+    type: 'SET_CURRENT_ROUTE',
+    routeIndex,
+  };
+};
