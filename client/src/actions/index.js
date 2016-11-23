388ce66a044c21b734a25bb7fc6ac656061a8cdb
mjstromberg <michaeljstromberg@gmail.com>
2016-11-22 23:15:31 -0800

    (refactor) Modify action creators to abstract out App component methods
    This drastically cleans up the App component and separates out concerns.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,34 +1,85 @@
-export const setDestination = (destination) => {
-  return {
-    type: 'SET_DESTINATION',
-    destination,
-  };
-};
+import axios from 'axios';
+import qs from 'qs';
 
+const serverUrl = /^(development|test)$/.test(process.env.NODE_ENV) ? 'http://localhost:3000' : '';
+
+/**
+ * Set destination to state
+ * @param  {Object} destination - The destination of the route. Must have keys `lat` and `lng`.
+ * @return {Object} action - The `SET_DESTINATION` action.
+ */
+export const setDestination = destination => ({
+  type: 'SET_DESTINATION',
+  destination,
+});
+
+/**
+ * Set origin to state (if origin is null, get user's geolocation)
+ * @param  {Object} origin - The origin of the route. Must have keys `lat` and 'lng'.
+ * @return {Function} callback - Using redux-thunk to return a callback that will
+ * either dispatch the 'SET_ORIGIN' action with the provided origin or use the
+ * user's current location.
+ */
 export const setOrigin = (origin) => {
-  return {
-    type: 'SET_ORIGIN',
+  return (dispatch) => {
+    if (!origin && 'geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        dispatch({
+          type: 'SET_ORIGIN',
+          origin: { lat: position.coords.latitude, lng: position.coords.longitude },
+        });
+      });
+    } else {
+      dispatch({
+        type: 'SET_CURRENT_ROUTE',
+        origin,
+      });
+    }
+  };
+};
+
+/**
+ * Set the interactionType to state
+ * @param  {String} interactionType - An enumerable string to determine the UI state.
+ * @return {Object} action - The `SET_INTERACTION_TYPE` action.
+ */
+export const setInteractionType = interactionType => ({
+  type: 'SET_INTERACTION_TYPE',
+  interactionType,
+});
+
+/**
+ * Set the routeIndex to state
+ * @param  {Number} routeIndex - This is an array index corresponding to a distinct
+ * route in the routeResponse object that was fetch from the Integration Server.
+ * @return {Object} action - The `SET_CURRENT_ROUTE` action.
+ */
+export const setCurrentRoute = routeIndex => ({
+  type: 'SET_CURRENT_ROUTE',
+  routeIndex,
+});
+
+/**
+ * GET request to Integration Server to retrieve route data
+ * @param  {Object} origin - The origin of the route. Must have keys `lat` and 'lng'.
+ * @param  {Object} destination - The end point. Must have keys `lat` and `lng`.
+ * @return {Function} callback - Using redux-thunk to return a callback that sends
+ * out a promisified, async request to the Integration Server.
+ */
+export const setRouteResponse = (origin, destination) => {
+  const queryString = qs.stringify({
     origin,
-  };
-};
+    destination,
+  });
 
-export const setInteractionType = (interactionType) => {
-  return {
-    type: 'SET_INTERACTION_TYPE',
-    interactionType,
-  };
-};
+  const request = axios.get(`${serverUrl}/api/trip?${queryString}`);
 
-export const setCurrentRoute = (routeIndex) => {
-  return {
-    type: 'SET_CURRENT_ROUTE',
-    routeIndex,
-  };
-};
-
-export const setRouteResponse = (routeResponse) => {
-  return {
-    type: 'SET_ROUTE_RESPONSE',
-    routeResponse,
+  return (dispatch) => {
+    request.then(({ data }) => {
+      dispatch({
+        type: 'SET_ROUTE_RESPONSE',
+        routeResponse: data,
+      });
+    });
   };
 };
