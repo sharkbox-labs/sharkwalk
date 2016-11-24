418e65acdd8d43dd467c6432e9d70b4e13809005
mjstromberg <michaeljstromberg@gmail.com>
2016-11-23 16:10:28 -0800

    (refactor) Add Drawer and Floating Action Button to App component
    Still need to wire these up with the correct props and associate them with the correct interactionType.

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,10 +1,13 @@
-import Map, { Marker } from 'google-maps-react';
 import React from 'react';
 import { Toolbar, ToolbarGroup } from 'material-ui/Toolbar';
 import IconButton from 'material-ui/IconButton';
 import SearchBarHamburgerIcon from 'material-ui/svg-icons/navigation/menu';
 import AutoComplete from 'material-ui/AutoComplete';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import Map, { Marker } from 'google-maps-react';
+import Drawer from 'material-ui/Drawer';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import MapsNavigation from 'material-ui/svg-icons/maps/navigation';
 import './App.css';
 import RiskPath from '../RiskPath/RiskPath';
 
@@ -15,7 +18,6 @@ const App = (props) => {
   const mapStyle = {};
   const appContainerStyle = {};
   const mapContainerStyle = {};
-
   const searchToolbarStyle = {
     backgroundColor: 'white',
     position: 'absolute',
@@ -40,9 +42,22 @@ const App = (props) => {
     // get search results for query
     // window.google.maps.places.SearchBox is a function...but don't know what it does
   };
+  // Create immutable interaction types for components to use
+  const interactionTypes = {
+    VIEWING_MAP: 'VIEWING_MAP',
+    SEARCHING_ORIGIN: 'SEARCHING_ORIGIN',
+    SEARCHING_DESTINATION: 'SEARCHING_DESTINATION',
+    SELECTING_ROUTE: 'SELECTING_ROUTE',
+    VIEWING_SIDEBAR: 'VIEWING_SIDEBAR',
+  };
 
   return (
-    <div className="app-container" style={appContainerStyle} >    
+    <div className="app-container" style={appContainerStyle} >
+      <Drawer
+        docked={false}
+        width={300}
+        open={props.interactionType === interactionTypes.VIEWING_SIDEBAR}
+      />
       <div className="map-container" style={mapContainerStyle}>
         <Map
           className="map"
@@ -83,11 +98,15 @@ const App = (props) => {
           />
         </ToolbarGroup>
       </Toolbar>
+      <FloatingActionButton className="floating-action-button-show">
+        <MapsNavigation />
+      </FloatingActionButton>
     </div>
   );
 };
 
 App.propTypes = {
+  interactionType: React.PropTypes.string.isRequired,
 };
 
 export default App;
