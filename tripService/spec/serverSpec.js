6ab0191c7b16784eaa0b38a3119e19adf0788e41
Analisa Heilmann <analisa.heilmann@gmail.com>
2016-11-23 17:00:32 -0800

    (test) Add server-side test.

--- tripService/spec/serverSpec.js
+++ tripService/spec/serverSpec.js
@@ -52,4 +52,20 @@ describe('Trip Server:', () => {
         done();
       });
   });
+  it('path should be have the same origin as the query', (done) => {
+    request(app)
+      .get('/trip')
+      .query(testQuery)
+      .end((error, response) => {
+        expect(error).to.not.exist;
+        expect(response.status).to.equal(200);
+        expect(response.body[0].path[0]).to.be.an('Array');
+        expect(response.body[0].path[0].length).to.be.equal(2);
+        expect(response.body[0].path[0][0]).to.equal(37.78364);
+        expect(response.body[0].path[0][1]).to.equal(-122.40918);
+        expect(response.body[1].path[0][0]).to.equal(37.78364);
+        expect(response.body[1].path[0][1]).to.equal(-122.40918);
+        done();
+      });
+  });
 });
