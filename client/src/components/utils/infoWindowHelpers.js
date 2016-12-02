2417c8352419b22700fb34aa74a83f8e7eef9773
Ker Moua <kermoua92@gmail.com>
2016-12-02 10:41:09 -0800

    (fix) Fix bug with 'faster/slower' and incorrect time

--- client/src/components/utils/infoWindowHelpers.js
+++ client/src/components/utils/infoWindowHelpers.js
@@ -1,14 +1,24 @@
 const getDurationDifference = (currentRouteDuration, alternateRouteDuration) => {
-  let durationDifference;
   const difference = currentRouteDuration - alternateRouteDuration;
-  const description = difference < 0 ? 'slower' : 'faster';
-  const absoluteDifference = Math.abs(difference);
+  let absoluteDifference;
 
-  if (absoluteDifference < 60) {
+  if (Math.abs(difference) > 60) {
+    // Get difference in minutes so that it is consistent with the total duration display
+    const differenceInMinutes = Math.floor(currentRouteDuration / 60) -
+                                Math.floor(alternateRouteDuration / 60);
+    absoluteDifference = Math.abs(differenceInMinutes);
+  } else {
+    absoluteDifference = Math.abs(difference);
+  }
+
+  const description = difference > 0 ? 'slower' : 'faster';
+
+  let durationDifference;
+
+  if (Math.abs(difference) < 60) {
     durationDifference = `${absoluteDifference} sec${(absoluteDifference !== 1 ? 's' : '')} ${description}`;
-  } else if (absoluteDifference < 60 * 60) {
-    const minutes = Math.floor(absoluteDifference / 60);
-    durationDifference = `${minutes}min ${description}`;
+  } else {
+    durationDifference = `${absoluteDifference}min ${description}`;
   }
 
   return durationDifference;
@@ -35,9 +45,10 @@ const getRiskDescription = (risk) => {
 };
 
 const displayRiskDifference = (currentRouteAvgRisk, alternateRouteAvgRisk) => {
-  // Risk description will be only displayed when the other route is selected so these should be in relation
-  // to the other route
-  const normalizedRiskDifference = Math.floor(((currentRouteAvgRisk - alternateRouteAvgRisk) / alternateRouteAvgRisk) * 100);
+  // Risk description will be only displayed when the other route is selected
+  // so it will be in relation to the other route
+  const normalizedRiskDifference =
+    Math.floor(((currentRouteAvgRisk - alternateRouteAvgRisk) / alternateRouteAvgRisk) * 100);
 
   return getRiskDescription(normalizedRiskDifference);
 };
@@ -46,9 +57,8 @@ const displayHoursMinutes = (seconds) => {
   const totalMinutes = Math.floor(seconds / 60);
   const hours = Math.floor(totalMinutes / 60);
   const minutesLeft = totalMinutes % 60;
-  const hoursDescription = hours > 1 ? 's' : '';
-  return hours > 0 ? `<span class='info-window-time'><b>${hours}hr${hoursDescription} ${minutesLeft}min</b></span>` :
-                     `<span class='info-window-time'><b>${minutesLeft} min</b></span>`;
+  return hours > 0 ? `<span class='info-window-time'><b>${hours}hr ${minutesLeft}min</b></span>` :
+                     `<span class='info-window-time'><b>${totalMinutes} min</b></span>`;
 };
 
 const displayMiles = meters => (Math.round(meters * 0.000621371));
@@ -59,4 +69,4 @@ export default {
   displayRiskDifference,
   displayHoursMinutes,
   displayMiles,
-}
+};
